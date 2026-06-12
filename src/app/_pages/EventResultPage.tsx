542683d4683ebb5784288cbe7596bc4788b5f0b2
GAL_son <gmail.com>
2026-06-12 19:30:45 +0200

    fix use client

--- src/app/_pages/EventResultPage.tsx
+++ src/app/_pages/EventResultPage.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Container, Button, Typography, Box, CircularProgress } from "@mui/material";
